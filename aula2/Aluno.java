4ecfd1716c8b46c668406a2243b2de096ec1be8b
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-12 09:27:24 -0300

    Atualizando

---
+++ aula2/Aluno.java
@@ -0,0 +1,21 @@
+// Criar classe. Puclic class, tem que ser o nome do arquivo, ou seja a classe que declarar no publico, tem que ser o nome do arquivo.
+
+// string nome;
+//short idade;
+//string genero;
+//string matricula;
+        //void estudar(){..}  - método, é um comportamento , uma ação. O metodo pode ser publico , privado, default. Void não tem retorno.
+        //void comer(){..}
+
+
+
+public class Aluno  {
+	// Nome da classe não pode ter espaço
+    // As características da classe são chamadas de atributos. Atributos precisam ter letra minúscula.
+    // Método de inicializacao do java. Toda vez que criar uma classe executavel. Precia criar o metodo main
+    // Quando tem o main, indica que o código começa no main
+    // Args é um parametro do metodo.
+	public static void main(String[] args){
+        System.out.println("Hellow World ! " + args[0]);
+    }
+}
